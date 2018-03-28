6ceaf87efcdbf5529b2884b29b09ef5ac7e642f4
esteban1234 <tva_libra@hotmail.com>
2018-03-28 14:31:45 -0600

    PAGINA LISTA

---
+++ index.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    
+    <link rel="icon" type="image/png" href="img/favicon.png" />
+    <title>SERVICIOS INMOBILIARIOS INTEGRALES GAZZO</title>
+   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
+    <META NAME="Title" content="SERVICIOS INMOBILIARIOS INTEGRALES GAZZO SA DE CV">
+    <META NAME="Author" CONTENT="[http://serviciosinmobiliariosgazzo.com/]"/>
+    <META name="Description" content="Venta de terrenos con facilidad de pago de hasta 30 meses sin intereses. ¡Contáctanos!"/>
+     <META NAME="Keywords" CONTENT="terrenos, Venta terrenos, terrenos en venta."/>
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/iconos.min.css">
+    <link rel="stylesheet" href="css/fontawesome-all.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/responsivo.css">
+    <link href="https://fonts.googleapis.com/css?family=Khula|Montserrat|Roboto" rel="stylesheet">
+    <script src="js/general.js"></script>
+
+
+
+    <script src="js/smooth-scroll.min.js"></script>
+    <script>
+      smoothScroll.init({
+      selector: '[data-scroll]',
+      selectorHeader: null,
+      speed: 2000,
+      easing: 'easeInOutCubic',
+      offset: 0,
+      callback: function ( anchor, toggle ) {}
+      });
+    </script>
+
+  </head>
+  <body>
+
+    <div class="banner"  id="INICIO">
+    <div class="fondo">
+      <div class="menu">
+        <nav class="pull-left">
+          <ul>
+            <a class="selector" id="select" data-scroll href="#INICIO" aria-pressed="true">INICIO</a>
+            <a class="selector" data-scroll href="#NOSOTROS" aria-pressed="true">NOSOTROS</a>
+            <a class="selector" data-scroll href="#SERVICIOS" aria-pressed="true">INMUEBLES</a>
+            <a class="selector" data-scroll href="#CONTACTO" aria-pressed="true">CONTACTO</a>
+          </ul>
+        </nav>
+
+        <figure class="logo pull-right">
+          <img src="img/gazzo.png" alt="GAZZO" class="img-responsive">
+        </figure>
+      </div>
+
+      <div class="textobanner pull-right">
+        <p class="uno">
+          Te ayudamos a CRECER haciendo realidad tus SUEÑOS
+        </p>
+        <p class="dos">Servicios inmobiliarios integrales</p>
+        <div class="">
+            <a class="btn icon-btn btn-warning" data-scroll href="#CONTACTO" aria-pressed="true"><span class=" btn-glyphicon fas fa-sign-out-alt img-circle text-warning"></span>CONTACTANOS</a>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+
+
+
+  <span class="ir-arriba"><i class="fa fa-arrow-up" aria-hidden="true"></i></span>
+
+<section class="seccion1" id="NOSOTROS" >
+
+<div class="section" style="padding: 5% 0%;">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <img src="img/nosotros.jpg"
+            class="img-responsive">
+          </div>
+          <div class="col-md-6">
+            <h1>SERVICIOS INMOBILIARIOS INTEGRALES GAZZO</h1>
+            <!-- <h3>A subtitle</h3> -->
+            <p>Somos una empresa inmobiliaria que tenemos la firme convicción de crear relaciones de valor a largo plazo para nuestros clientes y asociados. <br><br>
+
+            Estamos comprometidos en ofrecer un trato de calidad, honesto y transparente en los servicios que ofrecemos, siempre comprometidos en buscar el mejor acuerdo para todos nuestros clientes. En GAZZO Inmobiliaria buscamos ser tu primera opción para apoyarte en la promoción para la venta y renta de tus propiedades, contáctanos y conoce la nueva forma de hacer negocios inmobiliarios. Estamos seguros que podemos servirte como te mereces, siempre enfocados a encontrar el mejor acuerdo para ti. </p>
+          </div>
+        </div> <br><br><br>
+        <div class="row">
+          <div class="col-md-3">
+            <div class="iconos">
+              <i class="fas fa-home fa-5x"></i>
+            </div>
+            <h3 class="text-center">Inversión Segura</h3>
+          </div>
+          <div class="col-md-3">
+            <div class="iconos">
+              <i class="fas fa-book fa-5x"></i>
+            </div>
+            <h3 class="text-center">Guía para COMPRAR tu casa</h3>
+          </div>
+          <div class="col-md-3">
+            <div class="iconos">
+              <i class="far fa-handshake fa-5x"></i>
+            </div>
+            <h3 class="text-center">Negociación directa, Total Transparencia</h3>
+          </div>
+          <div class="col-md-3">
+            <div class="iconos">
+              <i class="fas fa-book fa-5x"></i>
+            </div>
+            <h3 class="text-center">Guía para VENDER tu casa</h3>
+          </div>
+        </div>
+
+      </div>
+    </div>
+ 
+
+</section>
+
+<section class="seccion2" id="SERVICIOS" style="background: #eceff1">
+
+  <div class="section" style="padding: 5% 0%;">
+      <div class="container">
+        <div class="row elimi-hr">
+          <h1 class="tit">Bienes inmuebles, tales como :</h1>
+      <hr> <br><br>
+          <div class="col-sm-6 col-md-3">
+            <div class="imagen">
+              
+            </div>
+            <h2 class="text-center tit-cetb">CASAS</h2>
+            
+          </div>
+          <div class="col-sm-6 col-md-3">
+            <div class="imagen2">
+              
+            </div>
+            <h2 class="text-center tit-cetb">EDIFICIOS</h2>
+            
+          </div>
+          <div class="col-sm-6 col-md-3">
+            <div class="imagen3">
+              
+            </div>
+            <h2 class="text-center tit-cetb">TERRENOS</h2>
+            
+          </div>
+          <div class="col-sm-6 col-md-3">
+            <div class="imagen4">
+              
+            </div>
+            <h2 class="text-center tit-cetb">BODEGAS</h2>
+            
+          </div>
+        </div> <br><br>
+         <div class="row brdr">
+          <div class="col-sm-4 col-md-4">
+            <h2 class="text-center tit-pifilmdrs" style="color: #000;">Protege tu dinero</h2>
+            <p class="text-center txt-pifilmdrs" style="color: #000;">Invertir te da la oportunidad de multiplicar tu dinero, evitando que desaparezca en gastos innecesarios.</p>
+          </div>
+          <div class="col-sm-4 col-md-4">
+            <h2 class="text-center tit-pifilmdrs" style="color: #000;">Inversión que trabaja por ti</h2>
+            <p class="text-center txt-pifilmdrs" style="color: #000;">Mientras tú disfrutas lo que te gusta hacer, tu inmueble genera plusvalía sin necesidad de meter ni las manos.</p>
+          </div>
+          <div class="col-sm-4 col-md-4">
+            <h2 class="text-center tit-pifilmdrs" style="color: #000;">Facilidad de compra</h2>
+            <p class="text-center txt-pifilmdrs" style="color: #000;">No tienes que realizar un esfuerzo extra, tu mayor trabajo será elegir tu inmueble.</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+</section>
+
+
+
+<section class="seccion3" id="CONTACTO" style="padding: 5% 0%;">
+
+            <h1 class="tit">CONTACTO</h1>
+      <hr> <br><br>
+
+  <div class="mapa">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3820.4471914757423!2d-93.17907048575672!3d16.75441262516231!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ecd97d977e5d03%3A0xf949657d7faf26d0!2sAgua+592%2C+La+Gloria%2C+29054+La+Gloria%2C+Chis.!5e0!3m2!1ses-419!2smx!4v1522266202016" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+  </div>
+
+
+<section class="container" style="padding: 5% 0% 0% 0%;">
+    <div class="row">
+        <div class="col-lg-5 mb-4">
+            <div class="card border-warning rounded-0">
+
+                <div class="card-header p-0">
+                    <div class="bg-warning text-white text-center" style="padding: 2%;">
+                        <h3>Formulario de Contacto</h3>
+                        <p class="m-0">Si tienes alguna duda o sugerencia, ponte en contacto con nosotros y con mucho gusto te asesoraremos.</p>
+                    </div>
+                </div>
+                <div class="card-body" style="padding: 3% 0%;">
+                    <div class="form-group">
+                        <label>Nombre Completo</label>
+                        <div class="input-group">
+                            <div class="input-group-addon bg-light"><i class="fa fa-user text-warning"></i></div>
+                            <input type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>Correo</label>
+                        <div class="input-group mb-2 mb-sm-0">
+                            <div class="input-group-addon bg-light"><i class="fa fa-envelope text-warning"></i></div>
+                            <input type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>Teléfono</label>
+                        <div class="input-group mb-2 mb-sm-0">
+                            <div class="input-group-addon bg-light"><i class="fa fa-phone prefix text-warning"></i></div>
+                            <input type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>Comentario</label>
+                        <div class="input-group mb-2 mb-sm-0">
+                            <div class="input-group-addon bg-light"><i class="far fa-edit text-warning"></i></div>
+                            <textarea class="form-control"></textarea>
+                        </div>
+                    </div>
+
+                    <div class="text-center">
+                        <button class="btn btn-warning btn-block rounded-0 py-2">ENVIAR</button>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+        <div class="col-lg-7">
+
+            <div class="row text-center">
+                <div class="col-md-6">
+                    <i class="fas fa-map-marker-alt fa-4x"></i> <br><br>
+                    <p>Av. Agua #592 Col. La Gloria,  <br>Tuxtla Gutierrez, Chiapas. C.P. 29038</p>
+                    
+                </div>
+
+                <div class="col-md-6">
+                    <i class="fas fa-phone-volume fa-4x"></i> <br><br>
+                    <p> PENDIENTE</p>
+                </div>
+            </div> <br><br>
+
+            <div class="row text-center">
+                <div class="col-md-6">
+                    <i class="far fa-envelope fa-4x"></i> <br><br>
+                    <p> info@serviciosinmobiliariosgazzo.com</p>
+                </div>
+                <div class="col-md-6">
+                    <i class="far fa-clock fa-4x"></i> <br><br>
+                    <p> Lunes a Viernes de 9 am a 6 pm</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+
+</section>
+
+
+<footer>
+<p>Derechos de autor 2018 © |  SERVICIOS INMOBILIARIOS INTEGRALES GAZZO SA DE CV</p>
+</footer>
+
+    <script src="js/sendCORREO.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/arriba.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/menu.js"></script>
+  </body>
+</html>
