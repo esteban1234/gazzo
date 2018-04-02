8b40fc0680cfbae81e459b083b6f54a96b6b59fd
esteban1234 <tva_libra@hotmail.com>
2018-04-02 18:10:23 -0500

    metas

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="es">
   <head>
     <meta charset="utf-8">
-    
+    <meta name="google-site-verification" content="SFfuxCwn6UnANbfR_vK2JjR8cIJfMvlJhLJAwZm1Lj4" />
     <link rel="icon" type="image/png" href="img/favicon.png" />
     <title>SERVICIOS INMOBILIARIOS INTEGRALES GAZZO</title>
    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
